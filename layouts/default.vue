56ed03c511eb8735396fc37ffde0d53f504d49d8
梦幻 <web2018_lang@163.com>
2020-02-07 21:16:19 +0800

    新建公共组件

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
+    <!-- 头部组件 -->
+    <Header/>
     <!-- 内容占位组件 -->
     <nuxt />
+    <!-- 页脚组件 -->
+    <Footer/>
   </div>
 </template>
 
 <script>
+//导入公共的组件
+import Header from "@/components/header"
+import Footer from "@/components/footer"
 export default {
+  //注册组件
+  components:{
+    Header,
+    Footer
+  }
 
 }
 </script>
