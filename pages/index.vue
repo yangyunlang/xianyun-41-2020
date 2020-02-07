9715b6d5debf8386a07059782344d66503456f84
梦幻 <web2018_lang@163.com>
2020-02-07 21:59:19 +0800

    首页轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,62 @@
 <template>
-  <div>
-    首页
-  </div>
+    <div class="container">
+        <!-- 轮播图 -->
+        <el-carousel 
+        :interval="5000" 
+        arrow="always">
+            <el-carousel-item 
+            v-for="(item, index) in banners" 
+            :key="index">
+                <div class="banner-image" 
+                :style="`
+                background:url(${$axios.defaults.baseURL}${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `">
+                </div>
+            </el-carousel-item>
+        </el-carousel>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            // 轮播图数据
+            banners: [
+                // {
+                //     url: "http://157.122.54.189:9095/assets/images/th03.jfif",
+                // },
+                // {
+                //     url: "http://157.122.54.189:9095/assets/images/th04.jfif",
+                // }
+            ]
+        }
+    },
+    mounted(){
+      this.$axios({
+        url:"/scenics/banners"
+      }).then(res => {
+        const { data } = res.data;
+        this.banners =data
+      })
+    }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
 
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
