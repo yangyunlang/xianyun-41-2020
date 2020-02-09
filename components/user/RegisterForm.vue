9d83cf57efd57ee11a22571c0ff0e6b10fe977a0
梦幻 <web2018_lang@163.com>
2020-02-09 22:44:52 +0800

    注册功能-3.表单验证

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -1,10 +1,10 @@
 <template>
   <el-form ref="form" :model="form" :rules="rules" class="form">
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="username">
       <el-input placeholder="用户名手机" v-model="form.username"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="captcha">
       <el-input placeholder="验证码" v-model="form.captcha">
         <template slot="append">
           <el-button @click="handleSendCaptcha">发送验证码</el-button>
@@ -12,14 +12,14 @@
       </el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="nickname">
       <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="password">
       <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
     </el-form-item>
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="checkPassword">
       <el-input placeholder="确认密码" type="password" v-model="form.checkPassword"></el-input>
     </el-form-item>
 
@@ -29,8 +29,33 @@
 <script>
 export default {
   data () {
+    //确认密码
+    //rule当前的规则
+    //value输入框的值
+    //callback 比如要执行，如果传入一个错误的对象，当前的字段就会报错
+    const validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.form.checkPassword !== "") {
+          //验证和确认密码是否一致，手动触发确认密码验证
+          this.$refs.form.validateField('checkPassword');
+        }
+        callback();
+      }
+    };
+
+    // 验证确认密码
+    const validateCheckPass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.form.password) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    };
     return {
-      //表单数据
       // 表单数据
       form: {
         username: "",   // 登录用户名/手机
@@ -39,8 +64,26 @@ export default {
         nickname: "",	// 昵称
         captcha: ""		// 手机验证码
       },
-      //表单规则
-      rules: {},
+      // 表单规则
+      rules: {
+        username: [
+          // required表示必填，message错误的提示信息. trigger输入框失去焦点时候触发验证
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        captcha: [
+          { required: true, message: '请输入验证码', trigger: 'blur' }
+        ],
+        nickname: [
+          { required: true, message: '请输入昵称', trigger: 'blur' }
+        ],
+        password: [
+          // validator是验证的函数，输入框失去焦点时候触发函数执行
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPassword: [
+          { validator: validateCheckPass, trigger: 'blur' }
+        ],
+      },
     }
   },
   methods: {
@@ -51,7 +94,14 @@ export default {
 
     //注册
     handleRegSubmit () {
-      console.log(this.form);
+      //   console.log(this.form);
+      this.$refs.form.validate(valid => {
+        //表单验证全部通过
+        if (valid) {
+          //调用注册的接口
+          console.log(this.form);
+        }
+      })
     }
   }
 }
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -34,7 +34,7 @@ import RegisterForm from "@/components/user/RegisterForm.vue"
 export default {
     data(){
         return {
-            currentTab: 0
+            currentTab: 1
         }
     },
     components:{
