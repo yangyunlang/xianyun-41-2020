d8c372089c4d605cb827c49e59587a0c34fc4eb6
梦幻 <web2018_lang@163.com>
2020-02-09 22:11:24 +0800

    注册功能-2.表单数据绑定

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -1,11 +1,11 @@
 <template>
   <el-form ref="form" :model="form" :rules="rules" class="form">
     <el-form-item class="form-item">
-      <el-input placeholder="用户名手机"></el-input>
+      <el-input placeholder="用户名手机" v-model="form.username"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="验证码">
+      <el-input placeholder="验证码" v-model="form.captcha">
         <template slot="append">
           <el-button @click="handleSendCaptcha">发送验证码</el-button>
         </template>
@@ -13,26 +13,32 @@
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="你的名字"></el-input>
+      <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item">
-      <el-input placeholder="密码" type="password"></el-input>
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
     </el-form-item>
     <el-form-item class="form-item">
-      <el-input placeholder="确认密码" type="password"></el-input>
+      <el-input placeholder="确认密码" type="password" v-model="form.checkPassword"></el-input>
     </el-form-item>
 
     <el-button class="submit" type="primary" @click="handleRegSubmit">注册</el-button>
   </el-form>
 </template>
-
 <script>
 export default {
   data () {
     return {
       //表单数据
-      form: {},
+      // 表单数据
+      form: {
+        username: "",   // 登录用户名/手机
+        password: "",    // 登录密码
+        checkPassword: "", // 确认密码
+        nickname: "",	// 昵称
+        captcha: ""		// 手机验证码
+      },
       //表单规则
       rules: {},
     }
