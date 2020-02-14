b1d403dfb3022c0a56344e733cb8f22d7f411305
梦幻 <web2018_lang@163.com>
2020-02-15 00:21:15 +0800

    撤销筛选

--- components/air/flightsFilters.vue
+++ components/air/flightsFilters.vue
@@ -172,7 +172,10 @@ export default {
 
     // 撤销条件时候触发
     handleFiltersCancel () {
-
+        this.airport = "";      //机场
+        this.flightTime = "";   //出发时间
+        this.company = "";      //航空公司
+        this.airSize = "";      //机型大小
     },
   }
 }
